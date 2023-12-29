456d8d1f458e3ff43ad4ccb00b640033cf3b3ba4
chengivan2 <chengoleivan@gmail.com>
2023-12-29 23:57:41 +0300

    Added the Download Counter DB, created super user and verified everythig is working well

--- bgremover/bgremoverapp/admin.py
+++ bgremover/bgremoverapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import DownloadCounter
 
 # Register your models here.
+admin.site.register(DownloadCounter)
--- bgremover/bgremoverapp/models.py
+++ bgremover/bgremoverapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class DownloadCounter(models.Model):
+    time = models.DateTimeField()
+    count = models.IntegerField()
